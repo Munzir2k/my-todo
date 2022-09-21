b10fb236dfdf6a678e6ac43afe4ed6b621f34366
Munzir2k <114055530+Munzir2k@users.noreply.github.com>
2022-09-21 13:23:52 +0530

    Update Contact.js

--- src/My_Components/Contact.js
+++ src/My_Components/Contact.js
@@ -4,9 +4,15 @@ export const Contact = () => {
   return (
     <div>
       <ul>
-        <p>E-mail: mohammadmunzir2k@gmail.com</p>
-        <p>Mobile: 7007693116</p>
-        <p>Address: C 19/196 Lallapura, Varanasi 221010</p>
+        <p>
+          <b>E-mail:</b> mohammadmunzir2k@gmail.com
+        </p>
+        <p>
+          <b>Mobile:</b> 7007693116
+        </p>
+        <p>
+          <b>Address:</b> C 19/196 Lallapura, Varanasi 221010
+        </p>
       </ul>
     </div>
   );
